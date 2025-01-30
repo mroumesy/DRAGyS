d8a76eaf221d0ec81497f0fc78e72c0f50af8e30
mroumesy <mroumesy@gmail.com>
2025-01-30 13:58:28 +0100

    Update __init__.py

--- python/dragys/__init__.py
+++ python/dragys/__init__.py
@@ -1,1 +1,7 @@
-from .DRAGyS import DRAGyS
+from dragys.dragys import Launcher
+
+__author__ = "Maxime Roumesy"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__copyright__ = "Copyright (C) 2025 Maxime Roumesy"
+__all__ = ["dragys", "tools"]
