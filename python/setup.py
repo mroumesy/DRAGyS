26edc35a67a5d40377f563e3bd376482604f4952
Maxime ROUMESY <mroumesy@gmail.com>
2025-02-04 11:45:41 +0100

    add PyQt6 as requirement install

--- python/setup.py
+++ python/setup.py
@@ -5,6 +5,9 @@ name='dragys',  # name of packageï¼ˆwhich will be shown when you type: pip listï
 version="1.0.0",  # version
 description='Disk Ring Adjusted Geometry yields Scattering phase function',  # description
 author='Maxime Roumesy',  # name of developer
-packages=find_packages(),  # list of models 
-license='MIT'  # licence 
+packages= find_packages(),  # list of models 
+license='MIT',  # licence 
+install_requires=[
+        'numpy','matplotlib', "PyQt6", "scipy", "astropy"
+        ]
 )
